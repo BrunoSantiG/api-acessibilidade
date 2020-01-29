eec494c54f77970caf05359283dbaebd908742c3
lucas-silva-dev <lucassilva.work@outlook.com>
2020-01-29 02:05:25 -0300

    add update usuario empresa

--- src/app/controllers/UsuarioEmpresaController.js
+++ src/app/controllers/UsuarioEmpresaController.js
@@ -82,8 +82,12 @@ class UsuarioEmpresaController {
         {model: Endereco, as: "Endereco"}],
     }).then((usuario_empresa) => {
       usuario_empresa.Usuario.senha=undefined;
+      let { id } = Usuario;
       return res.status(201).json({
         usuario:usuario_empresa.Usuario,
+        token: jwt.sign({ id }, authConfig.secret, {
+          expiresIn: authConfig.expiresIn
+        })
       })
     }).catch((err)=>{
       console.log("ERRO: "+err)
@@ -95,11 +99,86 @@ class UsuarioEmpresaController {
     const { page = 1 } = req.query;
 
     const empresas = await Usuario_Empresa.findAll({
+      attributes: ["cnpj", "razao_social", "telefone_fixo", "telefone_celular"],
+      include: [
+        {
+          model: Endereco,
+          as: "Endereco"
+        },
+        {
+          model: Usuario,
+          as: "Usuario",
+          attributes: ["nome", "email"]
+        }
+      ],
       limit: 20,
-      offset: (page - 1) * 20,
+      offset: (page - 1) * 20
     });
 
-    return res.json(empresas);
+    return res.status(201).json({ empresas });
+  }
+
+  async update(req, res) {
+    const schemaUsuario = Yup.object().shape({
+      nome: Yup.string(),
+      usuario: Yup.string(),
+      email: Yup.string().email()
+    });
+
+    const schemaEndereco = Yup.object().shape({
+      pais: Yup.string(),
+      estado: Yup.string(),
+      cidade: Yup.string(),
+      bairro: Yup.string(),
+      cep: Yup.number(),
+      logradouro: Yup.string(),
+      numero: Yup.number(),
+      complemento: Yup.string()
+    });
+
+    const schemaUsuarioEmpresa = Yup.object().shape({
+      cnpj: Yup.string(),
+      razao_social: Yup.string(),
+      telefone_fixo: Yup.number(),
+      telefone_celular: Yup.number()
+    });
+
+    if (!(await schemaUsuario.isValid(req.body.usuario))) {
+      return res
+        .status(400)
+        .json({ error: "Campo usuario não esta de acordo" });
+    }
+
+    if (!(await schemaEndereco.isValid(req.body.endereco))) {
+      return res
+        .status(400)
+        .json({ error: "Campo endereco não esta de acordo" });
+    }
+
+    if (!(await schemaUsuarioEmpresa.isValid(req.body.usuario_empresa))) {
+      return res
+        .status(400)
+        .json({ error: "Campo usuario_empresa não esta de acordo" });
+    }
+
+    const { id_usuario, id_endereco } = await Usuario_Empresa.findByPk(req.id_usuario);
+    const empresa = await Usuario_Empresa.findByPk(req.id_usuario);
+
+    const usuarioPk = await Usuario.findOne({
+      where: { id: id_usuario }
+    });
+    const enderecoPk = await Endereco.findOne({
+      where: { id: id_endereco }
+    });
+
+    const { usuario, usuario_empresa, endereco } = req.body;
+    const empresas = await empresa.update(
+      usuario_empresa,
+      usuarioPk.update(usuario),
+      enderecoPk.update(endereco)
+    );
+
+    return res.status(201).json({ empresas });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -11,13 +11,13 @@ import VagasController from './app/controllers/VagasController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+
 routes.post('/pcd', UsuarioPcdController.store);
 routes.post('/sessions', SessionController.store);
 routes.post('/freelancer', UsuarioFreelancerController.store);
 routes.post('/empresas', UsuarioEmpresaController.store);
 routes.post('/vagas', VagasController.store);
 
-routes.get('/empresas', UsuarioEmpresaController.index);
 
 routes.get('/usuario/usuarioExiste/:usuario', UsuarioController.UsuarioExists);
 routes.get('/usuario/emailExiste/:email', UsuarioController.EmailExists);
@@ -25,6 +25,8 @@ routes.get('/tipoDeficiencia', TipoDeficienciaController.listAll);
 
 
 routes.use(authMiddleware);
+routes.put("/empresas", UsuarioEmpresaController.update);
+routes.get('/empresas', UsuarioEmpresaController.index);
 routes.get('/pcd/usuario/:usuario', UsuarioPcdController.showByUsuario);
 routes.get('/pcd/id/:id', UsuarioPcdController.showById);
 routes.get('/pcd', UsuarioPcdController.index);
